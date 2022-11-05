27c9ded82f5b9069295b4ef029107b5288d0617e
nicomaza <nicolasmaza66@hotmail.com>
2022-11-05 16:02:59 -0300

    cargo formulario butacas

---
+++ FrontCine/Formularios/ButacasForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrontCine.Formularios
+{
+    public partial class ButacasForm : Form
+    {
+        public ButacasForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox31_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
