bbb45e1f54a4819fc7e0bfa035a0ed9d7b7eb0b2
MaximoGalindo <galindomaximo0@gmail.com>
2022-11-05 17:01:22 -0300

    commit

--- FrontCine/Formularios/InsertarPeliculas.Designer.cs
+++ FrontCine/Formularios/InsertarPeliculas.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FrontCine.Formularios
 {
-    partial class Peliculas
+    partial class InsertarPeliculas
     {
         /// <summary>
         /// Required designer variable.
@@ -28,17 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Peliculas
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1079, 608);
-            this.Name = "Peliculas";
-            this.Text = "Peliculas";
-            this.ResumeLayout(false);
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "InsertarPeliculas";
         }
 
         #endregion
