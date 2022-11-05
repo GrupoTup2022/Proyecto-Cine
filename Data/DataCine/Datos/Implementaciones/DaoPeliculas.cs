a71d7550c96a7a205c66b61c98bca0bd2d8d2c05
MaximoGalindo <galindomaximo0@gmail.com>
2022-11-05 16:54:52 -0300

    Commit Peliculas

--- Data/DataCine/Datos/Implementaciones/DaoPeliculas.cs
+++ Data/DataCine/Datos/Implementaciones/DaoPeliculas.cs
@@ -1,9 +1,11 @@
-﻿using APIRest_G9.Models.PeliculaConteiner;
+﻿using APIRest_G9.Models.PeliculaContainer;
+using APIRest_G9.Models.PeliculaConteiner;
 using DataCine.Datos.Interfaces;
 using DataCine.Dominio;
 using LibreriaTp;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ namespace DataCine.Datos.Implementaciones
 {
     public class DaoPeliculas : IDaoPeliculas
     {
+    
 
         public bool CargarPelicula(Pelicula oPelicula)
         {
@@ -32,6 +35,81 @@ namespace DataCine.Datos.Implementaciones
             else
                 return false;
 
+        }
+
+        public List<Distribuidora> CargarDistribuidora()
+        {
+            List<Distribuidora> distribuidoras = new List<Distribuidora>();
+            DataTable dt = HelperDAO.getinstancia().ConsultarDB("SP_Distribuidora");
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Distribuidora dis = new Distribuidora();
+                dis.Id = Convert.ToInt32(row[0].ToString());
+                dis.Nombre = row[1].ToString();
+                distribuidoras.Add(dis);
+            }
+            return distribuidoras;
+        }
+
+        public List<Clasificacion> ObtenerClasificaciones()
+        {
+            List<Clasificacion> lCla = new List<Clasificacion>();
+            DataTable dt = HelperDAO.getinstancia().ConsultarDB("SP_Clasificacion");
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Clasificacion cla = new Clasificacion();
+                cla.Id = Convert.ToInt32(row[0].ToString());
+                cla.Nombre = row[1].ToString();
+                lCla.Add(cla);
+            }
+            return lCla;
+        }
+
+        public List<Director> ObtenerDirectores()
+        {
+            List<Director> lDire = new List<Director>();
+            DataTable dt = HelperDAO.getinstancia().ConsultarDB("SP_Directores");
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Director dire = new Director();
+                dire.Id = Convert.ToInt32(row[0].ToString());
+                dire.Nombre = row[1].ToString();
+                lDire.Add(dire);
+            }
+            return lDire;
+        }
+
+        public List<Genero> ObtenerGeneros()
+        {
+            List<Genero> lGene = new List<Genero>();
+            DataTable dt = HelperDAO.getinstancia().ConsultarDB("SP_Genero");
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Genero gene = new Genero();
+                gene.Id = Convert.ToInt32(row[0].ToString());
+                gene.Nombre = row[1].ToString();
+                lGene.Add(gene);
+            }
+            return lGene;
+        }
+
+        public List<Pais> ObtenerPaises()
+        {
+            List<Pais> lPais= new List<Pais>();
+            DataTable dt = HelperDAO.getinstancia().ConsultarDB("SP_Pais");
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Pais pais = new Pais();
+                pais.Id = Convert.ToInt32(row[0].ToString());
+                pais.Nombre = row[1].ToString();
+                lPais.Add(pais);
+            }
+            return lPais;
         }
     }
 }
