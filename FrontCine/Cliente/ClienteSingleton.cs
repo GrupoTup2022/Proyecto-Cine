60e075528752d7f0b28651cc7906f78887eb1fd1
MaximoGalindo <galindomaximo0@gmail.com>
2022-11-06 16:02:43 -0300

    commit maxi

--- FrontCine/Cliente/ClienteSingleton.cs
+++ FrontCine/Cliente/ClienteSingleton.cs
@@ -30,6 +30,16 @@ namespace ReportesCine.Cliente
                 content = await result.Content.ReadAsStringAsync();
             return content;
         }
+
+        public async Task<string> PostAsync(string url, string data)
+        {
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            var result = await client.PostAsync(url, content);
+            var response = "";
+            if (result.IsSuccessStatusCode)
+                response = await result.Content.ReadAsStringAsync();
+            return response;
+        }
     }
 
 
--- FrontCine/Formularios/InsertarPeliculas.cs
+++ FrontCine/Formularios/InsertarPeliculas.cs
@@ -1,5 +1,8 @@
 ﻿using DataCine.Servicios.Implementacion;
 using DataCine.Servicios.Interfaces;
+using LibreriaTp;
+using Newtonsoft.Json;
+using ReportesCine.Cliente;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,20 +18,70 @@ namespace FrontCine.Formularios
     public partial class InsertarPeliculas : Form
     {
         private IPeliculasService service;
+        private Pelicula pelicula;
+
         public InsertarPeliculas()
         {
             InitializeComponent();
             service = new PeliculaService();
+            pelicula = new Pelicula();
         }
 
         private void InsertarPeliculas_Load(object sender, EventArgs e)
         {
-
+            CargarCBO(cboClasificaciones, "Clasificacion");
+            CargarCBO(cboDirectores, "Directores");
+            CargarCBO(cboGeneros, "Generos");
+            CargarCBO(cboDistribuidoras, "Distribuidora");
+            CargarCBO(cboPaises, "Paises");
         }
 
-        private async void CargarCBO(ComboBox cbo, List<object> lobjetos)
+        private async void CargarCBO(ComboBox cbo, string nombre)
         {
-            
+            string url = "https://localhost:7259/api/Peliculas/" + nombre;
+            var data = await ClienteSingleton.getinstancia().GetAsync(url);
+            List<object> lst = JsonConvert.DeserializeObject<List<object>>(data);
+
+            cbo.DataSource = lst;
+            cbo.ValueMember = "Id";
+            cbo.DisplayMember = "Nombre";
+        }
+
+        public async Task<bool> CargarPeliculaAsync(Pelicula pelicula)
+        {
+            string url = "https://localhost:7259/pelicula";
+            string cuentaJason = JsonConvert.SerializeObject(pelicula);
+            var data = await ClienteSingleton.getinstancia().PostAsync(url,cuentaJason);
+            return data == "true";
+        }
+
+        public async void ConfirmarPelicula()
+        {
+            Pelicula p = new Pelicula();
+            p.Titulo_local = txtTitulo.Text;
+            p.Titulo_original = null;
+            p.Descripcion = null;
+            p.pais.Id = Convert.ToInt32(cboPaises.SelectedValue);
+            p.clasificacion.Id = Convert.ToInt32(cboClasificaciones.SelectedValue);
+            p.Fecha_Estreno= Convert.ToDateTime(dtpEstreno.Value);
+            p.duracion = Convert.ToInt32(txtDuracion.Text);            
+            p.distribuidora.Id = Convert.ToInt32(cboDistribuidoras.SelectedValue);
+            p.genero.Id = Convert.ToInt32(cboGeneros.SelectedValue);
+            p.director.Id = Convert.ToInt32(cboDirectores.SelectedValue);
+            p.Baja = 0;
+
+
+            if(await CargarPeliculaAsync(p))
+            {
+                MessageBox.Show("Se registro correctamente la pelicula");  
+            }
+          
+               
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConfirmarPelicula();
         }
     }
 }
