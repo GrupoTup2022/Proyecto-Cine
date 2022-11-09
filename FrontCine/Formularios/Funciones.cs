b2fa1da621ab7927cacaae6e197a83a7eea68e42
MaximoGalindo <galindomaximo0@gmail.com>
2022-11-08 22:52:30 -0300

    Datos validados en consultar funciones

--- FrontCine/Formularios/Funciones.cs
+++ FrontCine/Formularios/Funciones.cs
@@ -26,12 +26,15 @@ namespace FrontCine.Formularios
 
         private async void Funciones_Load(object sender, EventArgs e)
         {
+            this.Enabled = false;
             await cargarPeliculas();
             await cargarHorarios();
             await cargarAudios();
             await cargarSalas();
             await cargarFunciones();
-
+            CBpeliculas.DropDownStyle = ComboBoxStyle.DropDownList;
+            CBpeliculas.SelectedIndex = -1;
+            this.Enabled = true;
 
         }
         private async Task cargarFunciones()
@@ -133,56 +136,60 @@ namespace FrontCine.Formularios
         private async void BTNconsultarFuncion_Click(object sender, EventArgs e)
         {
 
-
+            BTNconsultarFuncion.Enabled = false;
             await ConsultarFuncionFiltro();
-
+            CBpeliculas.SelectedIndex = -1;
+            BTNconsultarFuncion.Enabled = true;
 
         }
         private async Task ConsultarFuncionFiltro()
         {
-            int id_pelicula = (int)CBpeliculas.SelectedValue;
-            //int id_horario = (int)CBhorarios.SelectedValue;
-            //int id_audio = (int)CBaudios.SelectedValue;
-            //int id_sala = (int)CBsalas.SelectedValue;
-            //int precio = Convert.ToInt32(TXTprecio.Text);
-            DateTime fecha = DTPfecha.Value;
-
-
-            List<Funcion> lista = new List<Funcion>();
-            ParametroFuncion parametro = new ParametroFuncion();
-            parametro.id_pelicula = id_pelicula;
-            //parametro.id_horario = id_horario;
-            //parametro.id_audio = id_audio;
-            //parametro.id_sala = id_sala;
-            //parametro.precio = precio;
-            parametro.fecha = fecha;
-
-            string url = "https://localhost:7259/api/Funciones/FuncionesFiltro";
-            string funcionJSON = JsonConvert.SerializeObject(parametro);
-            var data = await ClienteSingleton.getinstancia().PostAsync(url, funcionJSON);
-
-            List<Funcion> lst = JsonConvert.DeserializeObject<List<Funcion>>(data);
-            dataGridView1.Rows.Clear();
-
-
-            if(lst.Count > 0)
+            if (ValidarDatos())
             {
+                int id_pelicula = (int)CBpeliculas.SelectedValue;
+                //int id_horario = (int)CBhorarios.SelectedValue;
+                //int id_audio = (int)CBaudios.SelectedValue;
+                //int id_sala = (int)CBsalas.SelectedValue;
+                //int precio = Convert.ToInt32(TXTprecio.Text);
+                DateTime fecha = DTPfecha.Value;
 
-                foreach (Funcion f in lst)
+
+                List<Funcion> lista = new List<Funcion>();
+                ParametroFuncion parametro = new ParametroFuncion();
+                parametro.id_pelicula = id_pelicula;
+                //parametro.id_horario = id_horario;
+                //parametro.id_audio = id_audio;
+                //parametro.id_sala = id_sala;
+                //parametro.precio = precio;
+                parametro.fecha = fecha;
+
+                string url = "https://localhost:7259/api/Funciones/FuncionesFiltro";
+                string funcionJSON = JsonConvert.SerializeObject(parametro);
+                var data = await ClienteSingleton.getinstancia().PostAsync(url, funcionJSON);
+
+                List<Funcion> lst = JsonConvert.DeserializeObject<List<Funcion>>(data);
+                dataGridView1.Rows.Clear();
+
+
+                if (lst.Count > 0)
                 {
-                    string id_funcion_ = f.Id.ToString();
-                    string nombre_pelicula_ = f.Pelicula.Titulo_local;
-                    string horario_funcion_ = f.Horario.Nombre.ToString();
-                    string audio_ = f.Audio.Nombre.ToString();
-                    string sala_ = f.Sala.Nombre.ToString();
-                    string precio_ = f.Precio.ToString();
-                    string fecha_ = f.fecha.ToString();
-                    dataGridView1.Rows.Add(id_funcion_, nombre_pelicula_, horario_funcion_, audio_, sala_, precio_, fecha_);
+
+                    foreach (Funcion f in lst)
+                    {
+                        string id_funcion_ = f.Id.ToString();
+                        string nombre_pelicula_ = f.Pelicula.Titulo_local;
+                        string horario_funcion_ = f.Horario.Nombre.ToString();
+                        string audio_ = f.Audio.Nombre.ToString();
+                        string sala_ = f.Sala.Nombre.ToString();
+                        string precio_ = f.Precio.ToString();
+                        string fecha_ = f.fecha.ToString();
+                        dataGridView1.Rows.Add(id_funcion_, nombre_pelicula_, horario_funcion_, audio_, sala_, precio_, fecha_);
+                    }
                 }
-            }
-            else
-            {
-                MessageBox.Show("No existen funciones con esos parametros");
+                else
+                {
+                    MessageBox.Show("No existen funciones con esos parametros");
+                }
             }
 
         }
@@ -216,7 +223,7 @@ namespace FrontCine.Formularios
                 if (MessageBox.Show("Desea dar de baja la funcion?", "ELIMINANDO FUNCION", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
                 {
 
-
+                    this.Enabled = false;
                     if (await bajaFuncion(id_funcion) != null)
                     {
                         MessageBox.Show("Funcion dada de baja correctamente");
@@ -227,19 +234,24 @@ namespace FrontCine.Formularios
                     {
                         MessageBox.Show("ERROR AL DAR DE BAJA LA FUNCION");
                     }
+                    this.Enabled = true;
                 }
             }
         }
 
         private async void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            this.Enabled = false;
             await DesabilitarFuncion();
+            this.Enabled = true;
         }
 
         private async void button1_Click(object sender, EventArgs e)
         {
+            button1.Enabled = false;
             dataGridView1.Rows.Clear();
             await cargarFunciones();
+            button1.Enabled = true;
         }
 
         private void BTNagregarFuncion_Click(object sender, EventArgs e)
@@ -252,6 +264,17 @@ namespace FrontCine.Formularios
            
 
         }
+
+        public bool ValidarDatos()
+        {
+            if(CBpeliculas.SelectedIndex == -1)
+            {
+                MessageBox.Show("Debe seleccionar una pelicula para consultar su funcion", "ADVERTENCIA");
+                return false;
+            }
+            return true;
+        }
+
     }
 }
 
